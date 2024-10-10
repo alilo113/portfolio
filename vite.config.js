2f2817f5239a9d43fad78970e6b8032b584d1dd6
unknown <aliopdandan@gmail.com>
2024-10-10 09:53:42 +0100

    f

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/protfolio', // Replace <your-repo-name> with your actual repository name
 });
