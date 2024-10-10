10af79a55fbe803bf70a1cc57bccec5b82efa2db
unknown <aliopdandan@gmail.com>
2024-10-10 09:44:16 +0100

    f

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/protfolio/', // Match the 'homepage' setting in package.json
+});
