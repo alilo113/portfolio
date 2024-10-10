c4772c303662e9065a6814f9d9f9daa490cec2d8
unknown <aliopdandan@gmail.com>
2024-10-10 09:47:58 +0100

    f

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/protfolio/', // Match the 'homepage' setting in package.json
+  plugins: [react()],
 });
